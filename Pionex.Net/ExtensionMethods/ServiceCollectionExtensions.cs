125c13d5a591eb53007cd3401690b37958a77815
Jkorf <jankorf91@gmail.com>
2026-09-15 10:05:31 +0200

    wip

--- Pionex.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Pionex.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -119,15 +119,16 @@ namespace Microsoft.Extensions.DependencyInjection
                     x.GetRequiredService<IOptions<PionexRestOptions>>(),
                     x.GetRequiredService<IOptions<PionexSocketOptions>>()));
 
-            services.AddTransient<IPionexSharedApiClient, PionexSharedApiClient>();
-
-            services.RegisterSharedApi(x => x.GetRequiredService<IPionexRestClient>().SpotApi.SharedApi);
-            services.RegisterSharedApi(x => x.GetRequiredService<IPionexSocketClient>().SpotApi.SharedApi);
-
-            services.RegisterSharedApiClientCapabilities<IPionexSharedApiClient>();
-
             services.RegisterSharedRestInterfaces(x => x.GetRequiredService<IPionexRestClient>().SpotApi.SharedClient);
             services.RegisterSharedSocketInterfaces(x => x.GetRequiredService<IPionexSocketClient>().SpotApi.SharedClient);
+
+            services.RegisterSharedApiClient<
+                IPionexSharedApiClient,
+                PionexSharedApiClient>(sharedApis => sharedApis
+                    .Add(client => client.SpotRest)
+                    .Add(client => client.SpotSocket)
+                    );
+
             return services;
         }
     }
